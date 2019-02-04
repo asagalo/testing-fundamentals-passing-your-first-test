560c32604a99545d4d34e7a92c1a2bb0df5e5576
Erico Pedroso <erico.pedroso@vindi.com.br>
2019-02-04 13:33:13 -0200

    fix tests

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,9 +3,11 @@ require_relative "../lib/person"
 describe Person do
   describe "#full_name" do
     it "returns the first and last names concatenated" do
-      # Write your spec here.
-      # You can delete the pending line.
-      pending "Not written yet"
+      person = Person.new(first_name: "Erico", last_name: "Pedroso")
+
+      full_name = person.full_name
+
+      expect(full_name).to eq("Erico Pedroso")
     end
   end
 end
